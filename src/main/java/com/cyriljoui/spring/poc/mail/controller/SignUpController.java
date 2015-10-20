53026b7e7b8a656a6d53f54fdc184b7d8e562a56
Cyril Joui <cyril.joui@gmail.com>
2015-10-20 15:26:16 +0200

    Mail templating with velocity Use messageBundle for subjects

--- src/main/java/com/cyriljoui/spring/poc/mail/controller/SignUpController.java
+++ src/main/java/com/cyriljoui/spring/poc/mail/controller/SignUpController.java
@@ -1,19 +1,33 @@
 package com.cyriljoui.spring.poc.mail.controller;
 
+import com.cyriljoui.spring.poc.mail.model.User;
+import org.apache.velocity.app.VelocityEngine;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailSender;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.context.MessageSource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.velocity.VelocityEngineUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.mail.internet.MimeMessage;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 @Controller
 public class SignUpController {
 
     @Autowired
-    private MailSender mailSender;
+    private JavaMailSender mailSender;
+
+    @Autowired
+    private VelocityEngine velocityEngine;
+
+    @Autowired
+    private MessageSource messageSource;
 
     @RequestMapping("/signup")
     public String signUp(Locale locale, Model model) {
@@ -21,21 +35,45 @@ public class SignUpController {
         // Locale locale = LocaleContextHolder.getLocale();
         model.addAttribute("locale", locale);
 
-        sendEmail(locale);
+        User user = new User();
+        user.setEmail("cyril.joui@gmail.com");
+        user.setFirstname("Cyril");
+        user.setLastname("Joui");
+        user.setLocale(locale);
+        sendEmail(user);
 
         return "signup";
     }
 
-    private void sendEmail(Locale locale) {
-        SimpleMailMessage message = new SimpleMailMessage();
+    private void sendEmail(User user) {
         String from = "poc-springmail@clip2.pro";
         String to = "cyril.joui@gmail.com";
-        String subject = "Subject Spring POC test java mail - Locale: " + locale;
-        String body = "Body mail ..." + locale;
-        message.setFrom(from);
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(body);
-        mailSender.send(message);
+        Locale locale = user.getLocale();
+        String subject = messageSource.getMessage("mail.registration.subject", new Object[] {user.getFirstname()}, locale);
+        MimeMessagePreparator preparator = mimeMessage -> {
+            MimeMessageHelper message = new MimeMessageHelper(mimeMessage);
+            message.setTo(to);
+            message.setFrom(from);
+            message.setSubject(subject);
+            Map<String, Object> model = new HashMap<>();
+            model.put("user", user);
+            String text = VelocityEngineUtils.mergeTemplateIntoString(
+                    velocityEngine, getI18nVelocityMailTemplate("signup.vm", locale), "utf-8", model);
+            message.setText(text, true);
+        };
+        this.mailSender.send(preparator);
+    }
+
+    private String getI18nVelocityMailTemplate(String mailFileTemplate, Locale locale) {
+        String language = locale.getLanguage();
+        String templateFolder = "/templates/mail/";
+        String templateFile = templateFolder + language + "/" + mailFileTemplate;
+
+        // Try template with locale suffix
+        if (getClass().getResource(templateFile) == null) {
+            // Default file has no suffix
+            templateFile = templateFolder + "en/" + mailFileTemplate;
+        }
+        return templateFile;
     }
 }
---
+++ src/main/java/com/cyriljoui/spring/poc/mail/model/User.java
@@ -0,0 +1,46 @@
+package com.cyriljoui.spring.poc.mail.model;
+
+import java.util.Locale;
+
+public class User {
+    private String firstname;
+
+    private String lastname;
+
+    private String email;
+
+    private Locale locale;
+
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public void setLocale(Locale locale) {
+        this.locale = locale;
+    }
+}
